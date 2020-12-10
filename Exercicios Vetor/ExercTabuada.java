f57267169637d485763375cc80bafe9d14d2e86e
yu-ri-i <75813083+yu-ri-i@users.noreply.github.com>
2020-12-10 19:52:23 -0300

    Update ExercTabuada.java

--- Exercicios Vetor/ExercTabuada.java
+++ Exercicios Vetor/ExercTabuada.java
@@ -3,18 +3,18 @@ public class ExercTabuada {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		final int E = 5;
-		int i, r, j, a[];
+		int i, r, i2, a[];
 		
 		a = new int[E];
 		
 		
-		for(j=0; j<=4; j++) {
+		for(i2=0; i2<=4; i2++) {
 			System.out.println("Insira um número para obter sua tabuada:");
-			a[j] = in.nextInt();
+			a[i2] = in.nextInt();
 			
-				for (i=0; i<=10; i++) {
-					r = a[j]*i;
-			System.out.println(a[j]+" x "+i+ " = "+r);
+			for (i=0; i<=10; i++) {
+			r = a[i2]*i;
+			System.out.println(a[i2]+"x"+i+ " = "+r);
 			
 			}
 		}
